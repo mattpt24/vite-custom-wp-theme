d8857058381da6ff2c15426a7b072e8ed20b2a9c
Matt Persell-Thompson <66426542+mattpt24@users.noreply.github.com>
2026-08-25 17:37:06 -0400

    Delete custom-wp-theme directory

--- custom-wp-theme/script.js
+++
@@ -1,1 +0,0 @@
-/* empty css     */console.log("Vite is successfully connected to your theme!");
--- custom-wp-theme/src/js/script.js
+++
@@ -1,4 +0,0 @@
-// Import SCSS so Vite tracks it for live reload and bundling
-import '../scss/style.scss';
-
-console.log('Vite is successfully connected to your theme!');
